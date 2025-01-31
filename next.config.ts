c6489a7048688f530635969eac8f565ac58523c0
Arsalan23c <arsalan23c@gmail.com>
2025-01-31 13:37:59 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  
 };
 
 export default nextConfig;
+
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
